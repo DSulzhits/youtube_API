970835b80030457dd8bec000fec4365d48c72686
DSulzhits <dsulzhits@gmail.com>
2023-02-28 16:04:38 +0300

    дописал код

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from class_channel.class_channel import Channel
 
 def main():
     green_grass = Channel('UCbaKd4mhqd1QvfDp5EaUlHw')
-    # print(green_grass.channel_info)
+    print(green_grass.channel_info)
     print(green_grass.channel_id)
     print(green_grass.channel_title)
     print(green_grass.channel_description)
@@ -10,7 +10,7 @@ def main():
     print(green_grass.channel_subscribers)
     print(green_grass.channel_videoCount)
     print(green_grass.channel_viewCount)
-    # green_grass.channel_id = 'Новое название'
+    green_grass.channel_id = 'Новое название'
     green_grass.make_json()
     vdud = Channel('UCMCgOm8GZkHp8zJ6l7_hIuA')
     vdud.make_json()
