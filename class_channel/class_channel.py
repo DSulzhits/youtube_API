b3e06c00a51ee8598f0a217e96c42a2ecacfa322
DSulzhits <dsulzhits@gmail.com>
2023-02-28 13:57:33 +0300

    добавлены методы класса

--- class_channel/class_channel.py
+++ class_channel/class_channel.py
@@ -2,16 +2,58 @@ import os
 from dotenv import load_dotenv
 import json
 from googleapiclient.discovery import build
+
 load_dotenv()
 
+
 class Channel:
+    api_key: str = os.getenv('YT_API_KEY')
+    youtube = build('youtube', 'v3', developerKey=api_key)
 
-    def __init__(self, ID):
-        self.ID = ID
+    def __init__(self, channel_id: str):
+        self.__id = channel_id
+        yt_channel = self.youtube.channels().list(id=self.__id, part='snippet,statistics').execute()
+        self.__channel_info = json.dumps(yt_channel, indent=2, ensure_ascii=False)
+        self.__info = json.loads(self.__channel_info)
+        self.__title = self.__info['items'][0]['snippet']['title']
+        self.__description = self.__info['items'][0]['snippet']['description']
+        self.__link = 'https://www.youtube.com/' + self.__info['items'][0]['snippet']['customUrl']
+        self.__subscribers = self.__info['items'][0]['statistics']['subscriberCount']
+        self.__videoCount = self.__info['items'][0]['statistics']['videoCount']
+        self.__viewCount = self.__info['items'][0]['statistics']['viewCount']
 
-    def print_info(self):
-        api_key: str = os.getenv('YT_API_KEY')
-        youtube = build('youtube', 'v3', developerKey=api_key)
-        channel = youtube.channels().list(id=self.ID, part='snippet,statistics').execute()
-        channel_info = json.dumps(channel, indent=2, ensure_ascii=False)
-        print(channel_info)
+    @property
+    def channel_id(self) -> str:
+        return self.__id
+
+    @property
+    def channel_title(self) -> str:
+        return self.__title
+
+    @property
+    def channel_description(self) -> str:
+        return self.__description
+
+    @property
+    def channel_link(self) -> str:
+        return self.__link
+
+    @property
+    def channel_subscribers(self) -> str:
+        return self.__subscribers
+
+    @property
+    def channel_videoCount(self) -> str:
+        return self.__videoCount
+
+    @property
+    def channel_viewCount(self) -> str:
+        return self.__viewCount
+
+    @property
+    def channel_info(self) -> str:
+        return self.__channel_info
+
+    @channel_id.setter
+    def channel_id(self, name_inp: str) -> None:
+        raise AttributeError("property 'channel_id' of 'Channel' object has no setter")
